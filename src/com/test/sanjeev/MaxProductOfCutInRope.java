c8a331e7e0fcda157fe963ad08dfd8183f47622a
Sanjeev Sinha <sanjeevsinha19@users.noreply.github.com>
2022-08-14 23:51:31 -0700

    Update and rename Solution2.java to MaxProductOfCutInRope.java

--- src/com/test/sanjeev/MaxProductOfCutInRope.java
+++ src/com/test/sanjeev/MaxProductOfCutInRope.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Solution2 {
+public class MaxProductOfCutInRope {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
